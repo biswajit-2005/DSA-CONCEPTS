c1638bc30e1f2bfd46cda0c902ef61bd6ffcd97c
Biswajit Baliarsingh <biswajitbaliarsingh777@gmail.com>
2025-11-09 20:33:20 +0530

    change in vector cpp

--- 1.STL/vector.cpp
+++ 1.STL/vector.cpp
@@ -4,33 +4,39 @@ using namespace std;
 
 int main()
 {
-    vector<int> v = {1, 2, 3, 4};
+    vector<int> v;
 
+    // Inserting elements
     v.push_back(10);
-    v.push_back(113);
-    v.push_back(16);
-    v.push_back(19);
-    v.push_back(15);
-    v.push_back(16);
+    v.push_back(20);
+    v.push_back(30);
 
-    for (int val : v)
-    {
-        cout << val << " ";
-    }
-    cout << endl;
+    cout << "Elements: ";
+    for (int i = 0; i < v.size(); i++)
+        cout << v[i] << " ";
+
+    cout << "\nFront: " << v.front();
+    cout << "\nBack: " << v.back();
+    cout << "\nSize: " << v.size();
+    cout << "\nCapacity: " << v.capacity();
 
     v.pop_back();
+    cout << "\nAfter pop_back(): ";
     for (int x : v)
-    {
         cout << x << " ";
-    }
-    cout << endl;
 
-    // using iterator
+    v.insert(v.begin() + 1, 15);
+    cout << "\nAfter insert: ";
+    for (int x : v)
+        cout << x << " ";
 
-    for (auto i = v.begin(); i != v.end(); i++)
-    {
-        cout << *i << "  ";
-    }
+    v.erase(v.begin());
+    cout << "\nAfter erase: ";
+    for (int x : v)
+        cout << x << " ";
+
+    v.clear();
+    cout << "\nAfter clear(), size: " << v.size() << endl;
+
     return 0;
 }
